67e0b48359284d9c960176a8802d2c4917dd27cd
Fantomchik <58704419+Fantomchik@users.noreply.github.com>
2024-03-14 15:14:53 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,10 +5,12 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Согласно правилу Шварцмана результат не должен привышать 3999!");
-        //System.out.print("Input: ");
+        System.out.print("Input: ");
         String expression = in.nextLine();
-        System.out.println(strToIntRome(expression));
-        //System.out.println("Output: " + calc(expression));
+        //System.out.println(intToStrRome(strToIntRome(expression)));
+
+        //System.out.println(strToIntRome(expression));
+        System.out.println("Output: " + calc(expression));
 
     }
 
@@ -25,11 +27,13 @@ public class Main {
         if (symbols.length < 3)
             throw new IndexOutOfBoundsException("строка не является математической операцией");
 
+
         try{
             Integer.parseInt(symbols[0]);
         }catch(Exception NumberFormatException){
             first = false;
         }
+
         try{
             Integer.parseInt(symbols[2]);
         }catch(Exception NumberFormatException){
@@ -43,13 +47,16 @@ public class Main {
 
 
 
+        boolean max = false;
 
         try{
             Integer.parseInt(symbols[0]);
             Integer.parseInt(symbols[2]);
 
+            if ((Integer.parseInt(symbols[0]) > 10) || (Integer.parseInt(symbols[2]) > 10))
+                max = true;
 
-            Integer.parseInt(symbols[2]);
+
 
             if (symbols[1].equals("+"))
                 result = Integer.toString(Integer.parseInt(symbols[0]) + Integer.parseInt(symbols[symbols.length - 1]));
@@ -66,105 +73,38 @@ public class Main {
 
         }catch (Exception NumberFormatException) {
 
+
+            if ((strToIntRome(symbols[0]) > 10))
+                throw new ArithmeticException("Введено число больше X");
+
+            if ((strToIntRome(symbols[2]) > 10))
+                throw new ArithmeticException("Введено число больше X");
+
             if (symbols[1].equals("+"))
-                result = Integer.toString(numberConversionToArab(symbols[0]) + numberConversionToArab(symbols[2]));
+                result = Integer.toString(strToIntRome(symbols[0]) + strToIntRome(symbols[2]));
 
             if (symbols[1].equals("-"))
-                result = Integer.toString(numberConversionToArab(symbols[0]) - numberConversionToArab(symbols[2]));
+                result = Integer.toString(strToIntRome(symbols[0]) - strToIntRome(symbols[2]));
 
             if (symbols[1].equals("*"))
-                result = Integer.toString(numberConversionToArab(symbols[0]) * numberConversionToArab(symbols[2]));
+                result = Integer.toString(strToIntRome(symbols[0]) * strToIntRome(symbols[2]));
 
             if (symbols[1].equals("/"))
-                result = Integer.toString(numberConversionToArab(symbols[0]) / numberConversionToArab(symbols[2]));
+                result = Integer.toString(strToIntRome(symbols[0]) / strToIntRome(symbols[2]));
 
             if (Integer.parseInt(result) < 0)
                 throw new ArithmeticException("в римской системе нет отрицательных чисел");
-            result = resultConversion(Integer.parseInt(result));
+            result = intToStrRome(Integer.parseInt(result));
 
 
 
         }
+
+        if (max)
+            throw new ArithmeticException("введено число больше 10");
 
         return result;
     }
-
-    private static int numberConversionToArab(String num){
-        switch (num) {
-            case "I":
-                return 1;
-
-            case "II":
-                return 2;
-
-            case "III":
-                return 3;
-
-            case "IV":
-                return 4;
-
-            case "V":
-                return 5;
-
-            case "VI":
-                return 6;
-
-            case "VII":
-                return 7;
-
-            case "VIII":
-                return 8;
-
-            case "IX":
-                return 9;
-
-            case "X":
-                return 10;
-
-            default:
-                return 0;
-
-        }
-    }
-
-    private static String numberConversionToRim(int num){
-        switch (num) {
-            case 1:
-                return "I";
-
-            case 2:
-                return "II";
-
-            case 3:
-                return "III";
-
-            case 4:
-                return "IV";
-
-            case 5:
-                return "V";
-
-            case 6:
-                return "VI";
-
-            case 7:
-                return "VII";
-
-            case 8:
-                return "VIII";
-
-            case 9:
-                return "IX";
-
-            case 10:
-                return "X";
-
-            default:
-                return "";
-
-        }
-    }
-
 
     private static int strToIntRome (String num){
         num = " " + num + " ";
@@ -219,46 +159,83 @@ public class Main {
         return kol;
     }
 
-    private static String resultConversion(int num){
+    private static String intToStrRome (int num){
         String result = "";
 
-        int c = num / 100;
-        int remainderС = num % 100;
+        if (num % 1000 > 0){
+            int i;
+            for (i =0; i < (num / 1000); i++ )
+                result += "M";
 
-        if (c == 1){
-            return result + "C";
-        }else {
-            int l = remainderС / 50;
-            int remainderL = remainderС % 50;
-
-            if (l == 1){
-                if (remainderL / 10 == 4){
-                    result += "XC";
-                    result += numberConversionToRim(remainderL % 10);
-                }else{
-                    result += "L";
-                    int x = remainderL / 10;
-                    for (int i = 0; i < x; i++){
-                        result += "X";
-                    }
-                    result += numberConversionToRim(remainderL % 10);
-                }
-            }else {
-                if (remainderL / 10 == 4){
-                    result += "XL";
-                    result += numberConversionToRim(remainderL % 10);
-                }else{
-                    int x = remainderL / 10;
-                    for (int i = 0; i < x; i++){
-                        result += "X";
-                    }
-                    result += numberConversionToRim(remainderL % 10);
-                }
-            }
-
-            return result;
+            num -= 1000 * i;
         }
 
+        if (num / 900 > 0){
+            result += "CM";
+            num -= 900;
+        }
 
+        if (num / 500 > 0){
+            result += "D";
+            num -= 500;
+        }
+
+        if (num / 400 > 0){
+            result += "CD";
+            num -= 400;
+        }
+
+        if (num / 100 > 0){
+            int i;
+            for (i = 0; i < (num / 100); i++)
+                result += "C";
+
+            num -= 100 * i;
+        }
+
+        if (num / 90 > 0){
+            result += "XC";
+            num -= 90;
+        }
+
+        if (num / 50 > 0){
+            result += "L";
+            num -= 50;
+        }
+
+        if (num / 40 > 0){
+            result += "XL";
+            num -= 40;
+        }
+
+        if (num / 10 > 0){
+            int i;
+            for (i = 0; i < (num / 10); i++)
+                result += "X";
+            num -= 10 * i;
+        }
+
+        if (num / 9 > 0){
+            result += "IX";
+            num -= 9;
+        }
+
+        if (num / 5 > 0){
+            result += "V";
+            num -= 5;
+        }
+
+        if (num / 4 > 0){
+            result += "IV";
+            num -= 4;
+        }
+
+        if (num > 0){
+            int i;
+            for (i = 0; i < num; i++)
+                result += "I";
+        }
+
+        return result;
     }
 }
